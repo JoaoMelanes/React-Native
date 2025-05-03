c64304294dfc1e20607e8290d91ca7902ec4f562
João Pedro <joaomelanes12@gmail.com>
2025-05-03 09:41:14 -0300

    push-repalce-navigate

--- navegacao/app/index.jsx
+++ navegacao/app/index.jsx
@@ -8,6 +8,8 @@ export default function Home(){
         <View style={[styles.container, {backgroundColor: "white"}]}>
             <Text>Home</Text>
             <Link push href={'/user'}>Ir para usuarios➡</Link>
+            <Link push href={'/products'}>Ir para produtos➡</Link>
+            <Link push href={'/error'}>Ir para error</Link>
         </View>
     )
 }
